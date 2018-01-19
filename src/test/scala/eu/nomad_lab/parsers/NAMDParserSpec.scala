2e69dd0fa12adc20d4a6fd154f2480cfb0405633
Berk Onat <b.onat@warwick.ac.uk>
2018-01-19 10:35:34 +0000

    Fixed scala test settings

--- src/test/scala/eu/nomad_lab/parsers/NAMDParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/NAMDParserSpec.scala
@@ -5,16 +5,16 @@ import org.specs2.mutable.Specification
 object NAMDParserTests extends Specification {
   "NAMDParserTest" >> {
     "[tiny with 507 atoms] test with json-events" >> {
-      ParserRun.parse(NAMDParser, "parsers/gromacs/test/examples/tiny/tiny.log", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(NAMDParser, "parsers/namd/test/examples/tiny/tiny.log", "json-events") must_== ParseResult.ParseSuccess
     }
     "[tiny with 507 atoms] test with json" >> {
-      ParserRun.parse(NAMDParser, "parsers/gromacs/test/examples/tiny/tiny.log", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(NAMDParser, "parsers/namd/test/examples/tiny/tiny.log", "json") must_== ParseResult.ParseSuccess
     }
     "[apoa1-trajectory with 92224 atoms] test with json-events" >> {
-      ParserRun.parse(NAMDParser, "parsers/gromacs/test/examples/apoa1-trajectory/apoa1.log", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(NAMDParser, "parsers/namd/test/examples/apoa1-trajectory/apoa1.log", "json-events") must_== ParseResult.ParseSuccess
     }
     "[apoa1-trajectory with 92224 atoms] test with json" >> {
-      ParserRun.parse(NAMDParser, "parsers/gromacs/test/examples/apoa1-trajectory/apoa1.log", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(NAMDParser, "parsers/namd/test/examples/apoa1-trajectory/apoa1.log", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
