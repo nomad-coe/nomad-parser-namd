b94bd5a9b2088fbd8b3165160240266b7779e025
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-05-09 14:53:20 +0200

    Implemented new basic parser

--- namdparser/__init__.py
+++ namdparser/__init__.py
@@ -16,4 +16,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from .NAMDParser import NamdParserInterface as NamdParser
+from .namd_parser import NAMDParser
